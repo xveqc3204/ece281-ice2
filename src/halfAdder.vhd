5470685e15b71a6f59d0efbcf54222a38cd644b8
xveqc3204 <c27john.alves@afacademy.af.edu>
2025-01-22 16:35:15 -0700

    Final Submission for ICE 2 VHDL Items

--- src/halfAdder.vhd
+++ src/halfAdder.vhd
@@ -9,11 +9,19 @@
 --| USAF Academy, CO 80840           \____//____/_/  |_/_/   /_/  |_|
 --| 
 --| ---------------------------------------------------------------------------
---|
+--| FILENAME      : halfAdder.vhd
+--| AUTHOR(S)     : John Alves
+--| CREATED       : 01/22/2025
 --| DESCRIPTION   : This file implements a one bit half adder.
 --|
+--| DOCUMENTATION : None
 --+----------------------------------------------------------------------------
+--| REQUIRED FILES :
 --|
+--|    Libraries : ieee
+--|    Packages  : std_logic_1164, numeric_std, unisim
+--|    Files     : NONE
+--+----------------------------------------------------------------------------
 --| NAMING CONVENSIONS :
 --|
 --|    i_<port name>            = on-chip input port
@@ -29,7 +37,8 @@ entity halfAdder is
   port(
 	i_A     : in  std_logic; -- 1-bit input port
 	i_B     : in  std_logic; 
-	o_S     : out std_logic  -- 1-bit output port (NOTE: NO semicolon on LAST port only!)	    
+	o_S     : out std_logic;  -- 1-bit output port (NOTE: NO semicolon on LAST port only!)	    
+	o_Cout  : out std_logic
 	-- TODO:  Carry port
   ); -- the semicolon is here instead
 end halfAdder;
@@ -43,5 +52,5 @@ begin
 	-- *concurrent* signal assignments
 	o_S    <= i_A xor i_B;
 	-- TODO:  Carry signal assignment
-	
+	O_Cout <= i_A and i_B;
 end halfAdder_arch;
--- src/halfAdder_tb.vhd
+++ src/halfAdder_tb.vhd
@@ -24,6 +24,10 @@ architecture test_bench of halfAdder_tb is
   component halfAdder is
 	port(
 		-- TODO: copy in port map from halfAdder.vhd
+        i_A     : in  std_logic; -- 1-bit input port
+        i_B     : in  std_logic; 
+        o_S     : out std_logic;  -- 1-bit output port (NOTE: NO semicolon on LAST port only!)	    
+        o_Cout  : out std_logic
 	); -- the semicolon is here instead	
   end component;
 
@@ -31,11 +35,11 @@ architecture test_bench of halfAdder_tb is
   -- declare signals needed to stimulate the UUT inputs
   signal w_sw1 : std_logic := '0';
   -- TODO:  sw0 signal
-  
+  signal w_sw0 : std_logic := '0';
   -- also need signals for the outputs of the UUT
   signal w_led1 : std_logic := '0';
   -- TODO:  led0 signal
-
+  signal w_led0 : std_logic := '0';
   
 begin
 	-- PORT MAPS ----------------------------------------
@@ -44,8 +48,9 @@ begin
 	halfAdder_inst : halfAdder port map (
 		i_A     => w_sw1, -- notice comma (not a semicolon)
 		i_B     => w_sw0,
-		o_S     => w_led0 -- no comma on LAST one
+		o_S     => w_led0, -- no comma on LAST one
 		-- TODO:  map Cout 
+		o_Cout  => w_led1
 	);
 
 
@@ -61,7 +66,15 @@ begin
             assert w_led0 = '0' report "bad sum" severity error;
             assert w_led1 = '0' report "bad carry" severity error;
 		-- TODO:  rest of test plan
-		
+		 w_sw1 <= '0'; w_sw0 <= '1'; wait for 10 ns;
+            assert w_led0 = '1' report "bad sum" severity error;
+            assert w_led1 = '0' report "bad carry" severity error;
+		 w_sw1 <= '1'; w_sw0 <= '0'; wait for 10 ns;
+            assert w_led0 = '1' report "bad sum" severity error;
+            assert w_led1 = '0' report "bad carry" severity error;
+		 w_sw1 <= '1'; w_sw0 <= '1'; wait for 10 ns;
+            assert w_led0 = '0' report "bad sum" severity error;
+            assert w_led1 = '1' report "bad carry" severity error;
 		wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
